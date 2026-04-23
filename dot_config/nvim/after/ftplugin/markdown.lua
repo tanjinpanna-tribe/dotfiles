c207b3adf7d5af88e776071d09ac27f18a70cd60
Bo Tembunkiart <bo.tembunkiart@tribe.ai>
2026-04-23 16:25:30 -0400

    feat(nvim): make markdown buffers easier to read

---
+++ dot_config/nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,6 @@
+vim.opt_local.wrap = true
+vim.opt_local.linebreak = true
+vim.opt_local.breakindent = true
+vim.opt_local.colorcolumn = ""
+vim.opt_local.textwidth = 0
+vim.opt_local.conceallevel = 2
